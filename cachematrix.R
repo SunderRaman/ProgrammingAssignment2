e253f375b067be6d421aa1403ab44e25b329a0ae
Sunder Raman Venkataraman <sunder.venkataraman@honeywell.com>
2017-07-23 15:52:32 +0530

    Add files via upload
    Commiting the 2nd Programming assignment related to Inversing the matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
-## Put comments here that give an overall description of what your
-## functions do
+## There are 2 functions makeCachematrix(get/set matrix & its invrese) and 
+## cacheSolve for returning the inverse from cache 
 
-## Write a short comment describing this function
+## This function gets and set a matrix, as well as its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
+  
+  invMatrix <- NULL
+  
+  # get the Matrix
+  get <- function() {
+    x
+  }
+  
+  # set the Matrix
+  set <- function(y) {
+    x <<- y
+    invMatrix <<- NULL
+  }
+  
+  # get the Inverse of the Matrix
+  getinverse <- function() {
+    invMatrix
+  }
 
+  # set the Inverse of the Matrix
+  setinverse <- function(inverseMatrix) 
+  {
+    invMatrix <<- inverseMatrix
+  }
+  
+  list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
 
-## Write a short comment describing this function
-
+# This function returns the inverse of the matrix and if 
+# already available, then picks from the cache
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  invMatrix <- x$getinverse()
+
+    if(!is.null(invMatrix)) {
+      message("Getting the inverse of the matrix from cache")
+      return(invMatrix)
+    }
+  # get the Current matrix
+  curMatrix <- x$get()
+  # inverse the matrix
+  invMatrix <- solve(curMatrix)
+  #set the inverse of the matrix
+  x$setinverse(invMatrix)
+  invMatrix
 }
